1d8d998ba5cdc63053cc7bcb631d948a6dc322a3
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-11-07 10:55:23 -0300

    Feat: Se añadió el componente Loader para mostrar un indicador de carga

---
+++ frontend/src/components/ui/loader.tsx
@@ -0,0 +1,17 @@
+export default function Loader() {
+    return (
+        <div className="containerLoader">
+            <div className="dot-spinner">
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+            </div>
+            <p>Cargando...</p>
+        </div>
+    );
+}
