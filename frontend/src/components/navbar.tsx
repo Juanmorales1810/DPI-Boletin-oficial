fadbdf637fa5021b899e4be17c13c6d01052dea0
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-11-07 13:05:51 -0300

    Feat: Se reestructuró la barra de navegación para mejorar la disposición de los elementos y la alineación

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -2,16 +2,18 @@ import { LogoCiDi, LogoSanJuanGob } from "./icons"
 
 export default function Navbar() {
     return (
-        <header className="sticky top-0 z-50 flex items-center justify-between bg-background h-16 py-4 px-8 sm:px-12 backdrop-blur-sm shadow-md">
-            <div className="flex items-center gap-4">
-                <a href="/" className="">
-                    <LogoSanJuanGob height={42} width={109} />
-                </a>
-            </div>
+        <header className="sticky top-0 z-50 flex justify-center items-center w-full bg-background h-16 py-2 px-8 shadow-md log:px-12">
+            <nav className="flex items-center justify-between gap-4 w-full max-w-6xl mx-auto">
+                <div className="flex items-center gap-4">
+                    <a href="/" className="">
+                        <LogoSanJuanGob height={42} width={109} />
+                    </a>
+                </div>
 
-            <div className="flex items-center gap-4">
-                <LogoCiDi height={39} width={109} />
-            </div>
+                <div className="flex items-center gap-4">
+                    <LogoCiDi height={39} width={109} />
+                </div>
+            </nav>
 
         </header>
     )
