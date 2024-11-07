3e0b76721d6f95bff08d6698c299e3d020f82941
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-11-07 13:06:45 -0300

    Feat: Se añadieron componentes de Tooltip, Toggle y ToggleGroup, y se mejoró el componente Loader con nuevas propiedades

--- frontend/src/components/ui/loader.tsx
+++ frontend/src/components/ui/loader.tsx
@@ -1,17 +1,31 @@
-export default function Loader() {
+import { cn } from "@/lib/utils";
+
+interface LoaderProps {
+    color?: string;
+    size?: string;
+    speed?: string;
+    placeholder?: string;
+}
+
+export default function Loader(props: LoaderProps) {
+    const { color, size, speed, placeholder } = props;
+    console.log("LoaderProps", props);
     return (
-        <div className="containerLoader">
-            <div className="dot-spinner">
-                <div className="dot-spinner__dot"></div>
-                <div className="dot-spinner__dot"></div>
-                <div className="dot-spinner__dot"></div>
-                <div className="dot-spinner__dot"></div>
-                <div className="dot-spinner__dot"></div>
-                <div className="dot-spinner__dot"></div>
-                <div className="dot-spinner__dot"></div>
-                <div className="dot-spinner__dot"></div>
+        <div className="flex flex-col justify-center items-center gap-[10px] font-medium">
+            <div className={cn(
+                `[--uib-size:2.8rem] [--uib-speed:0.9s] [--uib-color:#ff8300] relative flex items-center justify-start h-[var(--uib-size)] w-[var(--uib-size)]`)}>
+                <div className="dot-spinner__dot absolute top-0 left-0 flex items-center justify-start h-full w-full"></div>
+                <div className="dot-spinner__dot absolute top-0 left-0 flex items-center justify-start h-full w-full"></div>
+                <div className="dot-spinner__dot absolute top-0 left-0 flex items-center justify-start h-full w-full"></div>
+                <div className="dot-spinner__dot absolute top-0 left-0 flex items-center justify-start h-full w-full"></div>
+                <div className="dot-spinner__dot absolute top-0 left-0 flex items-center justify-start h-full w-full"></div>
+                <div className="dot-spinner__dot absolute top-0 left-0 flex items-center justify-start h-full w-full"></div>
+                <div className="dot-spinner__dot absolute top-0 left-0 flex items-center justify-start h-full w-full"></div>
+                <div className="dot-spinner__dot absolute top-0 left-0 flex items-center justify-start h-full w-full"></div>
             </div>
-            <p>Cargando...</p>
+            {
+                placeholder && <p>{placeholder}</p>
+            }
         </div>
     );
 }
