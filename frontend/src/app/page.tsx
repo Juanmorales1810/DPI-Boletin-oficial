8582e5515e15de6202f22254f5b39f47739db0db
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-11-11 09:15:43 -0300

    Feat: Se implementó un formulario para la creación de boletines con validación y manejo de errores

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import SubirArchivo from "@/components/subirArchivo";
-import TextEditor from "@/components/textEditor";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import Loader from "@/components/ui/loader";
 import {
   Select,
   SelectContent,
@@ -13,14 +7,59 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { boletinSchema } from "@/validations/boletinvalidation";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Separator } from "@/components/ui/separator";
-
-import { useState } from "react";
+import { cargarBoletin } from "@/store/appSilce";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRouter } from 'next/navigation';
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { toast } from "sonner";
+import { z } from "zod";
 
 export default function Home() {
-  const [tipoDocumento, setTipoDocumento] = useState("");
+
+  const dispatch = useDispatch();
+  const router = useRouter()
+
+  const form = useForm<z.infer<typeof boletinSchema>>({
+    resolver: zodResolver(boletinSchema),
+    defaultValues: {
+      nombre: "",
+      apellido: "",
+      tipoBoletin: undefined,
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof boletinSchema>) {
+    try {
+
+      toast.success("Crear Boletín", {
+        richColors: true,
+        style: {
+          padding: "16px",
+        },
+      });
+      dispatch(cargarBoletin({ ...values }));
+      router.push("/cargar-boletin")
+    } catch (error: any) {
+      console.error("Error en el envio: ", error);
+      toast.error(error.data?.detail, {
+        richColors: true,
+        style: {
+          padding: "16px",
+        },
+      });
+    }
+    console.log(values);
+  };
+
+
   return (
-    <section className="font-[family-name:var(--font-ubuntu)] py-10">
+    <section className="py-10">
 
       <div className="flex justify-center items-center mx-auto gap-10 py-20 bg-white rounded-lg shadow-md max-w-5xl">
         <div>
@@ -28,52 +67,70 @@ export default function Home() {
           <img src="/img/Frame- Escudo SJ.svg" alt="Frame- Escudo SJ" className="w-full" />
         </div>
         <Separator orientation="vertical" className="h-80" />
-        <div className="flex flex-col gap-4">
-          <div className="flex justify-center items-center gap-4">
-            <Label>Nombre</Label>
-            <Input className="shadow-md" />
-          </div>
-          <div className="flex justify-center items-center gap-4">
-            <Label>Apellido</Label>
-            <Input className="shadow-md" />
-          </div>
-          <Select
-            onValueChange={(value) => setTipoDocumento(value)}
-          >
-            <SelectTrigger className="bg-naranjaPrincipal text-white">
-              <SelectValue placeholder="Seleccione tipo de documento" />
-            </SelectTrigger>
-            <SelectContent className="bg-naranjaClaro">
-              <SelectItem value="leyes">Leyes</SelectItem>
-              <SelectItem value="decretos">Decretos</SelectItem>
-              <SelectItem value="normas">Normas</SelectItem>
-              <SelectItem value="ordenanzas">Ordenanzas</SelectItem>
-              <SelectItem value="notificaciones">Notificaciones(edictos)</SelectItem>
-              <SelectItem value="licitaciones">Licitaciones</SelectItem>
-              <SelectItem value="convocatorias">Convocatorias</SelectItem>
-              <SelectItem value="otro">Otro</SelectItem>
-            </SelectContent>
-          </Select>
-          <Button className="bg-naranjaPrincipal text-white w-24 ml-auto">
-            Siguiente
-          </Button>
-        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
+            <FormField
+              control={form.control}
+              name="nombre"
+              render={({ field }) => (
+                <FormItem className="flex flex-col justify-center items-center gap-1">
+                  <div className="flex justify-center items-center gap-2">
+                    <FormLabel>Nombre</FormLabel>
+                    <FormControl>
+                      <Input className="shadow-md" placeholder="Ingrese su nombre" {...field} />
+                    </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="apellido"
+              render={({ field }) => (
+                <FormItem className="flex flex-col justify-center items-center gap-1">
+                  <div className="flex justify-center items-center gap-2">
+                    <FormLabel className="">Apellido</FormLabel>
+                    <FormControl>
+                      <Input className="shadow-md" placeholder="Ingrese su apellido" {...field} />
+                    </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="tipoBoletin"
+              render={({ field }) => (
+                <FormItem>
+                  <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <FormControl>
+                      <SelectTrigger className="bg-naranjaPrincipal text-white">
+                        <SelectValue placeholder="Seleccione tipo de documento" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent className="bg-gris70">
+                      <SelectItem value="leyes">Leyes</SelectItem>
+                      <SelectItem value="decretos">Decretos</SelectItem>
+                      <SelectItem value="normas">Normas</SelectItem>
+                      <SelectItem value="ordenanzas">Ordenanzas</SelectItem>
+                      <SelectItem value="notificaciones">Notificaciones (edictos)</SelectItem>
+                      <SelectItem value="licitaciones">Licitaciones</SelectItem>
+                      <SelectItem value="convocatorias">Convocatorias</SelectItem>
+                      <SelectItem value="otro">Otro</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="bg-naranjaPrincipal text-white w-24 ml-auto">
+              Siguiente
+            </Button>
+          </form>
+        </Form>
       </div>
-      {
-        tipoDocumento === "notificaciones" ? (
-          <SubirArchivo
-            disabled={false}
-            id="acta_constitutiva"
-            checkbox={false}
-            // apiSlice={postArchivo}
-            nombreSA={"nombreSA"}
-            pathArchivo={"archivo?.path_arch"}
-          />
-        ) : (
-          <TextEditor />
-        )
-      }
-      <Loader color="F2f2f2" size="24px" speed="1s" />
     </section>
   );
 }
