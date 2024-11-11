b4b4ba6a9696576983c9e82633889e53ae65b98b
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-11-11 09:18:31 -0300

    Fix: Se reemplazó el elemento <a> por <Link> en la barra de navegación para mejorar la navegación en Next.js

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { LogoCiDi, LogoSanJuanGob } from "./icons"
 
 export default function Navbar() {
@@ -5,11 +6,10 @@ export default function Navbar() {
         <header className="sticky top-0 z-50 flex justify-center items-center w-full bg-background h-16 py-2 px-8 shadow-md log:px-12">
             <nav className="flex items-center justify-between gap-4 w-full max-w-6xl mx-auto">
                 <div className="flex items-center gap-4">
-                    <a href="/" className="">
+                    <Link href="/" className="">
                         <LogoSanJuanGob height={42} width={109} />
-                    </a>
+                    </Link>
                 </div>
-
                 <div className="flex items-center gap-4">
                     <LogoCiDi height={39} width={109} />
                 </div>
