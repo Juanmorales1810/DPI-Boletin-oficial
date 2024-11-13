19add279b39fc3db41dd02cbde015bbefc9d743a
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-11-13 11:51:36 -0300

    Refactor: Ajustar el espaciado en el layout de carga del boletín

--- frontend/src/app/cargar-boletin/layout.tsx
+++ frontend/src/app/cargar-boletin/layout.tsx
@@ -4,7 +4,7 @@ export default function CargaLayout({
     children: React.ReactNode;
 }) {
     return (
-        <section className="flex flex-col items-center justify-center gap-4 pt-16">
+        <section className="flex flex-col items-center justify-center gap-4 pt-4">
             <div className="flex flex-col items-center justify-center w-full">
                 {children}
             </div>
