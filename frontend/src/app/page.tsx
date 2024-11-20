ffae2ea759b114cf838e6b735bb5b27b3f98bb7b
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-11-20 11:44:42 -0300

    Fix: Actualizar enlaces y mejorar el diseño de la página de boletín oficial

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,34 +7,34 @@ export default function BoletinOficialPage() {
         {
             title: 'Leyes y Decretos oficiales',
             description: 'Consulta todos los boletines de Leyes y Decretos oficiales.',
-            link: '/boletin-oficial/legislacion-avisos-oficiales',
+            link: '/boletin-oficial/leyes-decretos-oficiales',
         },
         {
             title: 'Normas y Ordenanzas',
             description: 'Consulta todos los boletines de Normas y Ordenanzas.',
-            link: '/boletin-oficial/legislacion-avisos-oficiales',
+            link: '/boletin-oficial/normas-y-ordenanzas',
         },
         {
             title: 'Edictos y Licitaciones',
             description: 'Consulta todos los boletines de Edictos y Licitaciones.',
-            link: '/boletin-oficial/legislacion-avisos-oficiales',
+            link: '/boletin-oficial/edictos-y-licitaciones',
         },
         {
             title: 'Convocatorias y otros',
             description: 'Consulta todos los boletines de Convocatorias y otros.',
-            link: '/boletin-oficial/legislacion-avisos-oficiales',
+            link: '/boletin-oficial/convocatorias-y-otros',
         }
     ];
 
     return (
         <div className="w-full text-grisPrincipal">
-            <div className="h-[300px] relative sm:h-[150px] w-full flex justify-center px-12 sm:px-20 md:px-32 lg:px-[222px] bg-[url('/img/textura.jpg')] bg-cover bg-no-repeat -z-10 bg-naranjaPrincipal"></div>
+            <div className="h-[150px] relative sm:h-[150px] w-full flex justify-center px-12 sm:px-20 md:px-32 lg:px-[222px] bg-[url('/img/textura.jpg')] bg-cover bg-no-repeat -z-10 bg-naranjaPrincipal"></div>
             <div className="mt-[-60px] flex w-full justify-center px-4 sm:px-20 md:px-32 lg:px-[120px] max-w-7xl mx-auto">
                 <div className="w-full bg-white border-2 px-6 md:px-10 lg:px-32 py-9 flex flex-col rounded-lg shadow-md">
-                    <h1 className="flex-grow-0 flex-shrink-0 text-2xl md:text-3xl font-bold text-center text-grisPrincipal">Boletin oficial</h1>
+                    <h1 className="flex-grow-0 flex-shrink-0 text-2xl md:text-3xl font-bold text-center text-grisPrincipal">Boletín oficial</h1>
                 </div>
             </div>
-            <div className="max-w-6xl grid grid-cols-1 md:grid-cols-2 gap-4 mx-auto mt-8 py-6">
+            <div className="max-w-6xl grid grid-cols-1 md:grid-cols-2 gap-4 mx-auto mt-8 py-6 px-2">
                 {cards.map((card, index) => (
                     <Link
                         key={index}
@@ -46,7 +46,7 @@ export default function BoletinOficialPage() {
                     </Link>
                 ))}
             </div>
-            <div className='max-w-6xl mx-auto py-4'>
+            <div className='max-w-6xl mx-auto py-4 px-2'>
                 <Button
                     asChild
                     className="bg-naranjaPrincipal text-white"
