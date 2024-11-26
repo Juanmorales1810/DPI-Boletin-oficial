7bb1ae913a44ba80c6cdefdb2e4e57f334729abf
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-11-26 12:05:08 -0300

    se agregó un esquema de validación para el boletín, incluyendo nuevos campos como título, descripción, tipo de actividad y duración de publicación, mejorando la integridad de los datos

--- frontend/src/validations/boletinvalidation.ts
+++ frontend/src/validations/boletinvalidation.ts
@@ -23,10 +23,42 @@ export const mappedBoletin: { [key in TypeBoletin]: string } = {
     convocatorias: "Convocatorias",
     otro: "Otro",
 };
+const typeActividad = [
+    "Minera",
+    "Agrícola",
+    "Industrial",
+    "Comercial",
+    "Turística",
+] as const;
+
+export type TypeActividad = (typeof typeActividad)[number];
+
+export const mappedActividad: { [key in TypeActividad]: string } = {
+    Minera: "Minera",
+    Agrícola: "Agrícola",
+    Industrial: "Industrial",
+    Comercial: "Comercial",
+    Turística: "Turística",
+};
+const typeDuracion = ["1", "2", "3", "4", "5", "6", "7", "14", "21"] as const;
+
+export type TypeDuracion = (typeof typeDuracion)[number];
+
+export const mappedDuracion: { [key in TypeDuracion]: string } = {
+    1: "1 día",
+    2: "2 días",
+    3: "3 días",
+    4: "4 días",
+    5: "5 días",
+    6: "6 días",
+    7: "1 semana",
+    14: "2 semanas",
+    21: "3 semanas",
+};
 
 // Definimos el esquema de validación para el formulario de usuario
 export const boletinSchema = z.object({
-    nombre: z
+    titulo: z
         .string()
         .min(3, {
             message: "El nombre debe tener al menos 3 caracteres.",
@@ -34,10 +66,30 @@ export const boletinSchema = z.object({
         .max(70, {
             message: "El nombre no debe exceder los 70 caracteres.",
         }),
-    email: z.string().email({
-        message: "Debes ingresar un email válido.",
-    }),
+    descripcion: z
+        .string()
+        .min(3, {
+            message: "La descripción debe tener al menos 3 caracteres.",
+        })
+        .max(500, {
+            message: "La descripción no debe exceder los 500 caracteres.",
+        }),
     tipoPublicacion: z.enum(typeBoletin, {
-        errorMap: () => ({ message: "Debes seleccionar un tipo de sociedad." }),
+        errorMap: () => ({
+            message: "Debes seleccionar un tipo de Publicación.",
+        }),
     }),
+    tipoActividad: z.enum(typeActividad, {
+        errorMap: () => ({
+            message: "Debes seleccionar un tipo de actividad.",
+        }),
+    }),
+    duracionPublicacion: z.enum(typeDuracion, {
+        errorMap: () => ({
+            message: "Debes seleccionar una duración de publicación.",
+        }),
+    }),
+    // calendario: z.array(z.date()).min(1, {
+    //     message: "Debes seleccionar al menos una fecha.",
+    // }),
 });
