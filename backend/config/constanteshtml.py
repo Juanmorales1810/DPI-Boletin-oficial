1b529599bee09888407b6bc0539304d3e03b5f8f
NP <133921604+Franmarr@users.noreply.github.com>
2024-11-27 11:37:00 -0300

    actualizacion a constantes html

--- backend/config/constanteshtml.py
+++ backend/config/constanteshtml.py
@@ -1,6 +1,18 @@
-ALLOWED_TAGS = ['b', 'i', 'u', 'a', 'p', 'strong', 'em']
+ALLOWED_TAGS = [
+    'b', 'i', 'u', 'a', 'p', 'strong', 'em',
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',  # Headers
+    'span',                               # Inline styling/formatting
+    'div',                               # Block-level container
+    'ul', 'ol', 'li',                    # Lists
+    'br',                                # Line breaks
+    'img',                               # Images (since you have img attributes)
+    'table', 'thead', 'tbody', 'tr', 'th', 'td'  # Tables
+]
 
 ALLOWED_ATTRIBUTES = {
     'a': ['href', 'title', 'rel'],
-    'img': ['src', 'alt']
+    'img': ['src', 'alt'],
+    'span': ['class'],
+    'div': ['class'],
+    'p': ['class']
 }
