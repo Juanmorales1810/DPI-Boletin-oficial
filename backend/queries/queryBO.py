6af437b9ef7aeac23facfa21d9a7aa1899f64c62
NP <133921604+Franmarr@users.noreply.github.com>
2024-11-29 12:05:26 -0300

    Nueva funcion y muchos cambios en la forma de trabajar los archivos
    Primero se movio el seek(0) que estaba al momento de crear el directorio hacia la funcion donde se hace el read(extraer texto), y luego se creo una nueva funcion cargar pdf, donde recibe el archivo pdf y el boletin refrescado que se creo anteriormente, se actualiza el nombre del archivo y el path y luego se crea el directorio en la misma funcion. Podria agregar mas validaciones o manejo de errores

--- backend/queries/queryBO.py
+++ backend/queries/queryBO.py
@@ -39,32 +39,41 @@ async def subir_boletin(boletinData: BoletinCreate, session: Session):
     return boletin
 
 
-
-async def buscar_mas_tipos(session: Session, tipoPublicacion: Optional[list[str]], tituloBO: Optional[str]= None, fechaInicio: Optional[str]= None, page: int=1, pageSize: int=10):
+async def cargar_pdf(session:Session, archivo: BytesIO, boletin: Boletin):
     try:
-        query= select(Boletin) #selecciona todos los boletines
-        if fechaInicio:
-            fechaConvertida= datetime.strptime(fechaInicio, "%Y-%m-%d")
-            inicioDia=fechaConvertida.replace(hour=0, minute=0, second=0, microsecond=0) # a la fecha convertida, se le asigna el horario de inicio de dia
-            finDia=fechaConvertida.replace(hour=23, minute=59, second=59, microsecond=999999)# a la fecha convertida, se le asigna el horario de fin de dia
-            query= query.where(Boletin.fechaPublicacion.between(inicioDia, finDia)) #selecciona los boletines que se publicaron entre el inicio y fin del dia. Between es un metodo de SQLModel que permite seleccionar los valores que estan entre dos valores, equivale a usar el operador >= y <=
-        if tipoPublicacion:
-            query = query.where(Boletin.tipoPublicacion.in_(tipoPublicacion))
-        if tituloBO:
-            query = query.where(Boletin.titulo.like(f"%{tituloBO}%")) # con el metodo like, puedo buscar parcialmente la palabra, por ejemplo de Juan, busco ju y me devuelve de la db quien se llame Juan
-
-        total_query = select(func.count()).select_from(query.subquery())  # func.count() es una funcion de SQL que cuenta el numero de filas que cumple con la condicion pero sin la paginacion. select_from es un metodo de SQLModel que selecciona las filas de una tabla, en este caso, de la subconsulta que se hace con query.subquery(), una subconsulta se hace para no afectar a la consulta principal
-        total = session.exec(total_query).one() #ejecuta la consulta y devuelve el resultado, en este caso, el numero de filas que cumple con la condicion y espera un unico resultado
-            
-        query = query.order_by(Boletin.fechaPublicacion.desc()) #ordena los boletines por fecha de publicacion de manera descendente
-        query = query.offset((page - 1) * pageSize).limit(pageSize)
-
-        result = session.exec(query).all() #ejecuta toda la consulta y devuelve una lista con los resultados? Si, gracias al .all()
-        
-        return result, total #result es del tipo list[Boletin] que es una lista de objetos de la clase Boletin
+        boletin.nombreArchivo=f"{boletin.id}_boletin.pdf" #.name es un atributo de BytesIO que devuelve el nombre del archivo por ejemplo, si el archivo es "hola.pdf", devuelve "hola.pdf" y es diferente a .filename solo porque no es una instancia de UploadFile
+        boletin.pathArchivo=str(await crear_path(boletin.nombreArchivo))
+        session.add(boletin)
+        session.commit()
+        session.refresh(boletin)
     except Exception as e:
         session.rollback()
         raise e
+    crear_directorio(archivo, boletin.nombreArchivo)
+
+    return boletin
+    
+    
+async def extraer_texto_pdf(file: UploadFile):
+    contenidoPDF=await file.read()
+    pdf= PdfReader(BytesIO(contenidoPDF))#pdfReader es una funcion de PyPDF2 que permite leer un archivo PDF, en este caso, BytesIO(contenidoPDF) es un archivo PDF en memoria
+    textoExtraido="" #inicializo la variable textoExtraido
+
+    for page in pdf.pages:
+        textoExtraido+= page.extract_text()
+    
+    contadorPalabras= len(textoExtraido.split()) #split es un metodo de Python que separa una cadena de texto en palabras, por defecto, separa por espacios
+    await file.seek(0)
+
+    return textoExtraido, contadorPalabras
+
+async def calcular_pdf(contador:int):
+    precio:float=0
+    precio= contador*8
+
+    return precio
+
+
 
 def html_a_pdf(html: str) -> BytesIO:
     pdf = BytesIO()
@@ -111,10 +120,12 @@ async def crear_directorio(archivo: UploadFile, nombreArch:str):
         
         rutaDirec.mkdir(parents=True, exist_ok=True)
 
-        await archivo.seek(0)
 
         with rutaArch.open("wb") as f:
-            content= await archivo.read()
+            if isinstance(archivo, UploadFile):
+                content= await archivo.read()
+            else:
+                content= archivo.read()
             f.write(content)
         #archivo.file.seek(0)#seek es un metodo de Python que mueve el cursor al inicio del archivo, lo hice ya que al leer el archivo aca, luego quedaba inutilizable para la siguiente funcnion en el router
 
@@ -135,25 +146,33 @@ async def buscar_boletin_por_id(session: Session, id: int):
     return boletin
 
 
-async def extraer_texto_pdf(file: UploadFile):
-    contenidoPDF=await file.read()
-    pdf= PdfReader(BytesIO(contenidoPDF))#pdfReader es una funcion de PyPDF2 que permite leer un archivo PDF, en este caso, BytesIO(contenidoPDF) es un archivo PDF en memoria
-    textoExtraido="" #inicializo la variable textoExtraido
-
-    for page in pdf.pages:
-        textoExtraido+= page.extract_text()
-    
-    contadorPalabras= len(textoExtraido.split()) #split es un metodo de Python que separa una cadena de texto en palabras, por defecto, separa por espacios
-
-    return textoExtraido, contadorPalabras
-
-async def calcular_pdf(contador:int):
-    precio:float=0
-    precio= contador*8
-
-    return precio
 
 
+async def buscar_mas_tipos(session: Session, tipoPublicacion: Optional[list[str]], tituloBO: Optional[str]= None, fechaInicio: Optional[str]= None, page: int=1, pageSize: int=10):
+    try:
+        query= select(Boletin) #selecciona todos los boletines
+        if fechaInicio:
+            fechaConvertida= datetime.strptime(fechaInicio, "%Y-%m-%d")
+            inicioDia=fechaConvertida.replace(hour=0, minute=0, second=0, microsecond=0) # a la fecha convertida, se le asigna el horario de inicio de dia
+            finDia=fechaConvertida.replace(hour=23, minute=59, second=59, microsecond=999999)# a la fecha convertida, se le asigna el horario de fin de dia
+            query= query.where(Boletin.fechaPublicacion.between(inicioDia, finDia)) #selecciona los boletines que se publicaron entre el inicio y fin del dia. Between es un metodo de SQLModel que permite seleccionar los valores que estan entre dos valores, equivale a usar el operador >= y <=
+        if tipoPublicacion:
+            query = query.where(Boletin.tipoPublicacion.in_(tipoPublicacion))
+        if tituloBO:
+            query = query.where(Boletin.titulo.like(f"%{tituloBO}%")) # con el metodo like, puedo buscar parcialmente la palabra, por ejemplo de Juan, busco ju y me devuelve de la db quien se llame Juan
+
+        total_query = select(func.count()).select_from(query.subquery())  # func.count() es una funcion de SQL que cuenta el numero de filas que cumple con la condicion pero sin la paginacion. select_from es un metodo de SQLModel que selecciona las filas de una tabla, en este caso, de la subconsulta que se hace con query.subquery(), una subconsulta se hace para no afectar a la consulta principal
+        total = session.exec(total_query).one() #ejecuta la consulta y devuelve el resultado, en este caso, el numero de filas que cumple con la condicion y espera un unico resultado
+            
+        query = query.order_by(Boletin.fechaPublicacion.desc()) #ordena los boletines por fecha de publicacion de manera descendente
+        query = query.offset((page - 1) * pageSize).limit(pageSize)
+
+        result = session.exec(query).all() #ejecuta toda la consulta y devuelve una lista con los resultados? Si, gracias al .all()
+        
+        return result, total #result es del tipo list[Boletin] que es una lista de objetos de la clase Boletin
+    except Exception as e:
+        session.rollback()
+        raise e
 
 
 
