d8f490c70f70bab185bb52088d4d928cbedba5a7
Julydnl <julylymrt@gmail.com>
2024-12-02 10:03:25 -0300

    Feat: Mejora de metadata y configuración de Open Graph para el boletín oficial

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,9 +4,9 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { Ubuntu } from 'next/font/google'
 import type { Metadata } from "next";
+import { Toaster } from "sonner";
 
 import "@/style/globals.css"
-import { Toaster } from "sonner";
 
 const ubuntu = Ubuntu({
   subsets: ["latin"],
@@ -14,10 +14,44 @@ const ubuntu = Ubuntu({
   weight: ["300", "400", "500", "700"],
 });
 
-
 export const metadata: Metadata = {
-  title: "Boletín Oficial",
+  title: {
+    default: "Boletín Oficial",
+    template: `%s - Boletín Oficial`,
+  },
   description: "Boletín Oficial de San Juan Gobierno",
+  keywords: ["Adopción", "Mascotas", "Perros", "Gatos", "Adopta", "Adopta una mascota", "Adopta un perro", "Adopta un gato", "Adopta un animal", "Adopta un amigo", "Adopta un compañero", "Adopta un compañero", "Cuidados", "Veranarías Cercanas", "Cuidados animales"],
+  icons: {
+    icon: "/logo-san-juan.svg",
+    apple: "/logo-san-juan.svg",
+  },
+  manifest: "/manifest.json",
+  creator: "Juan Morales",
+  openGraph: {
+    title: "Boletín Oficial",
+    description: "Boletín Oficial de San Juan Gobierno",
+    url: 'https://pichirika.com',
+    siteName: 'PichiriKa',
+    images: [
+      {
+        url: 'https://pichirika.com/Metadata.jpg', // Must be an absolute URL
+        width: 1200,
+        height: 630,
+      },
+
+    ],
+    locale: 'es_AR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Boletín Oficial",
+    description: "Boletín Oficial de San Juan Gobierno",
+    siteId: '1467726470533754880',
+    creator: '@Juanmora1810',
+    creatorId: '1467726470533754880',
+    images: ['https://pichirika.com/Metadata.jpg'], // Must be an absolute URL
+  },
 };
 
 export default function RootLayout({
