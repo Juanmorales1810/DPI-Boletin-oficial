6639d2d99c7dfeb8531ca6b4c435e8bfec646dc4
NP <133921604+Franmarr@users.noreply.github.com>
2024-12-27 08:35:10 -0300

    adaptacion inicio db para async

--- backend/config/db.py
+++ backend/config/db.py
@@ -1,12 +1,24 @@
 from sqlmodel import SQLModel, create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 #DATABASE_URL = "sqlite:///database.db"
-engine = create_engine(os.getenv("DATABASE_URL"), echo=True)
+# engine = create_engine(os.getenv("DATABASE_URL"), echo=True)
 
 
 
-def init_db():
-    SQLModel.metadata.create_all(engine)
+# def init_db():
+#     SQLModel.metadata.create_all(engine)
+
+async_engine = create_async_engine(os.getenv("DATABASE_URL"), echo=True, future=True)
+async_session = async_sessionmaker(bind=async_engine, expire_on_commit=False, class_=AsyncSession)
+
+async def init_db():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session():
+    async with async_session() as session:
+        yield session
