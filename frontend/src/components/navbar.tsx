258d37849977ef6904de4dcf67306764972d1e56
Juanmorales1810 <juan1810.jm@gmail.com>
2025-05-13 12:54:49 -0300

    Refactor: Mejorar la legibilidad del código en el componente Navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,30 +1,54 @@
 "use client";
 
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from './ui/navigation-menu';
-import { Sheet, SheetContent, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
-import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
-import { LogoCiDi, LogoSanJuanGob } from "./icons"
-import { Button } from '@/components/ui/button'
-import { Menu } from "lucide-react"
-import { useState } from "react"
-import Link from "next/link"
-import React from 'react';
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+} from "./ui/navigation-menu";
+import {
+    Sheet,
+    SheetContent,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { LogoCiDi, LogoSanJuanGob } from "./icons";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { useState } from "react";
+import Link from "next/link";
+import React from "react";
 
 const linksBoletin = [
-    { title: "Leyes y Decretos oficiales", href: "/boletin-oficial/leyes-decretos-oficiales" },
-    { title: "Normas y Ordenanzas", href: "/boletin-oficial/normas-y-ordenanzas" },
-    { title: "Edictos y Licitaciones", href: "/boletin-oficial/edictos-y-licitaciones" },
-    { title: "Convocatorias y otros", href: "/boletin-oficial/convocatorias-y-otros" }
-]
+    {
+        title: "Leyes y Decretos oficiales",
+        href: "/boletin-oficial/leyes-decretos-oficiales",
+    },
+    {
+        title: "Normas y Ordenanzas",
+        href: "/boletin-oficial/normas-y-ordenanzas",
+    },
+    {
+        title: "Edictos y Licitaciones",
+        href: "/boletin-oficial/edictos-y-licitaciones",
+    },
+    {
+        title: "Convocatorias y otros",
+        href: "/boletin-oficial/convocatorias-y-otros",
+    },
+];
 
 const linksCrear = [
     { title: "Publicar en el boletin ofical", href: "/cargar-boletin" },
-    { title: "Requisitos", href: "/requisitos" },
-    { title: "Crear un aviso", href: "/" }
-]
+    { title: "Requisitos", href: "/boletin-oficial/requisitos" },
+    { title: "Crear un aviso", href: "/" },
+];
 
 export default function Navbar() {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <header className="sticky top-0 z-50 flex justify-center items-center w-full bg-background h-16 py-2 px-8 shadow-md">
             <nav className="flex items-center justify-between gap-4 w-full max-w-7xl mx-auto">
@@ -37,7 +61,9 @@ export default function Navbar() {
                     <NavigationMenu>
                         <NavigationMenuList>
                             <NavigationMenuItem>
-                                <NavigationMenuTrigger className='text-grisPrincipal font-semibold hover:text-naranjaPrincipal'>Boletín Oficial</NavigationMenuTrigger>
+                                <NavigationMenuTrigger className="text-grisPrincipal font-semibold hover:text-naranjaPrincipal">
+                                    Boletín Oficial
+                                </NavigationMenuTrigger>
                                 <NavigationMenuContent>
                                     <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
                                         {linksBoletin.map((item) => (
@@ -51,7 +77,9 @@ export default function Navbar() {
                                 </NavigationMenuContent>
                             </NavigationMenuItem>
                             <NavigationMenuItem>
-                                <NavigationMenuTrigger className='text-grisPrincipal font-semibold hover:text-naranjaPrincipal'>Publicar</NavigationMenuTrigger>
+                                <NavigationMenuTrigger className="text-grisPrincipal font-semibold hover:text-naranjaPrincipal">
+                                    Publicar
+                                </NavigationMenuTrigger>
                                 <NavigationMenuContent>
                                     <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
                                         {linksCrear.map((item) => (
@@ -78,33 +106,41 @@ export default function Navbar() {
                                 <span className="sr-only">Abrir menú</span>
                             </Button>
                         </SheetTrigger>
-                        <SheetContent side="right" className="w-[240px] sm:w-[300px]">
+                        <SheetContent
+                            side="right"
+                            className="w-[240px] sm:w-[300px]"
+                        >
                             <VisuallyHidden>
                                 <SheetTitle>Menú de navegación</SheetTitle>
                             </VisuallyHidden>
                             <nav className="flex flex-col space-y-4 mt-4">
-                                {
-                                    linksBoletin.map((item) => (
-                                        <Link key={item.title} href={item.href} className="hover:bg-accent hover:text-accent-foreground px-3 py-2 rounded-md text-sm font-medium" onClick={() => setIsOpen(false)}>
-                                            {item.title}
-                                        </Link>
-                                    ))
-                                }
-                                {
-                                    linksCrear.map((item) => (
-                                        <Link key={item.title} href={item.href} className="hover:bg-accent hover:text-accent-foreground px-3 py-2 rounded-md text-sm font-medium" onClick={() => setIsOpen(false)}>
-                                            {item.title}
-                                        </Link>
-                                    ))
-                                }
+                                {linksBoletin.map((item) => (
+                                    <Link
+                                        key={item.title}
+                                        href={item.href}
+                                        className="hover:bg-accent hover:text-accent-foreground px-3 py-2 rounded-md text-sm font-medium"
+                                        onClick={() => setIsOpen(false)}
+                                    >
+                                        {item.title}
+                                    </Link>
+                                ))}
+                                {linksCrear.map((item) => (
+                                    <Link
+                                        key={item.title}
+                                        href={item.href}
+                                        className="hover:bg-accent hover:text-accent-foreground px-3 py-2 rounded-md text-sm font-medium"
+                                        onClick={() => setIsOpen(false)}
+                                    >
+                                        {item.title}
+                                    </Link>
+                                ))}
                             </nav>
                         </SheetContent>
                     </Sheet>
                 </div>
             </nav>
-
         </header>
-    )
+    );
 }
 const ListItem = React.forwardRef<
     React.ElementRef<"a">,
@@ -118,13 +154,15 @@ const ListItem = React.forwardRef<
                     className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
                     {...props}
                 >
-                    <div className="text-sm font-medium leading-none">{title}</div>
+                    <div className="text-sm font-medium leading-none">
+                        {title}
+                    </div>
                     <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
                         {children}
                     </p>
                 </a>
             </NavigationMenuLink>
         </li>
-    )
-})
-ListItem.displayName = "ListItem"
+    );
+});
+ListItem.displayName = "ListItem";
